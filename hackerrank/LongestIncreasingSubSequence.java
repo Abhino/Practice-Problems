a6708ebe21ed4c893fe11a53507eca7cf9938273
abhino <abhino@gmail.com>
2015-03-28 23:51:12 -0400

    Added longest increasing subsequence problem: Hackerrank

---
+++ hackerrank/LongestIncreasingSubSequence.java
@@ -0,0 +1,35 @@
+/*https://www.hackerrank.com/challenges/longest-increasing-subsequent*/
+import java.util.Scanner;
+
+class LongestIncreasingSubSequence{
+
+  static int longestSubSequence(int arr[], int n)
+  {
+    int i,j,max = 0;
+    int list[] = new int[n];
+     
+     for ( i = 0; i < n; i++ )
+        list[i] = 1;
+      
+     for ( i = 1; i < n; i++ )
+        for ( j = 0; j < i; j++ )
+           if ( arr[i] > arr[j] && list[i] < list[j] + 1)
+              list[i] = list[j] + 1;
+      
+     for ( i = 0; i < n; i++ )
+        if ( max < list[i] )
+           max = list[i];   
+     return max;
+  }
+   
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+    for (int i=0;i<n;i++) {
+      arr[i] = sc.nextInt();
+    }
+    sc.close();
+    System.out.println(longestSubSequence(arr,n));        
+  }
+}
