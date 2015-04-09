073a3bbf39124f8939247c6295ad3b94e9d816a2
abhino <abhino@gmail.com>
2015-04-09 18:34:00 -0400

    Added Maximizing XOR: Hackerrank problem

---
+++ hackerrank/BitwiseXOR.java
@@ -0,0 +1,31 @@
+/*https://www.hackerrank.com/challenges/maximizing-xor*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BitwiseXOR {
+	
+	static int XOR(int num1,int num2){
+		return (num1 ^ num2);
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		
+		BufferedReader ob = new BufferedReader(new InputStreamReader(System.in));
+		int num1 = Integer.parseInt(ob.readLine());
+		int num2 = Integer.parseInt(ob.readLine());
+		int max = 0,xor,i,j;
+		for(i = num1;i<=num2;i++){
+			for(j = num1;j<=num2;j++){
+				xor = XOR(i,j);
+				if(xor > max)
+					max = xor; 
+			}
+		}
+		System.out.println(max);
+
+	}
+
+}
