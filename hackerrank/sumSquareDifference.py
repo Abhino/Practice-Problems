06f15f6980d3baa9fa2a0aec98c345a9aad5725a
abhino <abhino@gmail.com>
2015-04-17 23:58:04 -0400

    Added SumSquare Project Euler problem

---
+++ hackerrank/sumSquareDifference.py
@@ -0,0 +1,15 @@
+def diffSeries(b):
+    n = int(b)
+    sum2 = (n*(n+1))/2
+    sum2 = sum2*sum2
+    sum3 = (n*(n+1)*(2*n+1))/6     
+    diff = sum2 - sum3
+    return diff
+
+if __name__ == '__main__':
+    t = raw_input()
+    for x in xrange(0,int(t)):
+        n = int(raw_input())
+        diff = diffSeries(n)
+        print diff
+
